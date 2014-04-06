1fd3493a3443d3634176ebcc8202588317395416
Faruq Rasid <faruq@digital-connect.net>
2014-04-06 11:27:44 +0800

    Revert "Fixed the string processing when persisting java.sql.Timestamp classes."
    This reverts commit 51105ee4dd3d55908167dbccff30e1bbcb792868.

--- src/main/java/com/j256/ormlite/db/SqliteAndroidDatabaseType.java
+++ src/main/java/com/j256/ormlite/db/SqliteAndroidDatabaseType.java
@@ -4,10 +4,9 @@ import java.sql.SQLException;
 
 import com.j256.ormlite.android.DatabaseTableConfigUtil;
 import com.j256.ormlite.field.DataPersister;
+import com.j256.ormlite.field.FieldConverter;
 import com.j256.ormlite.field.FieldType;
 import com.j256.ormlite.field.types.DateStringType;
-import com.j256.ormlite.field.types.TimeStampStringType;
-import com.j256.ormlite.field.types.TimeStampType;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.DatabaseTableConfig;
 
@@ -51,20 +50,13 @@ public class SqliteAndroidDatabaseType extends BaseSqliteDatabaseType {
 	}
 
 	@Override
-	public DataPersister getDataPersister(DataPersister defaultPersister) {
-		if (defaultPersister == null) {
-			return super.getDataPersister(null);
-		}
+	public FieldConverter getFieldConverter(DataPersister dataPersister) {
 		// we are only overriding certain types
-		switch (defaultPersister.getSqlType()) {
+		switch (dataPersister.getSqlType()) {
 			case DATE :
-				if (defaultPersister instanceof TimeStampType) {
-					return TimeStampStringType.getSingleton();
-				} else {
-					return DateStringType.getSingleton();
-				}
+				return DateStringType.getSingleton();
 			default :
-				return super.getDataPersister(defaultPersister);
+				return super.getFieldConverter(dataPersister);
 		}
 	}
 
